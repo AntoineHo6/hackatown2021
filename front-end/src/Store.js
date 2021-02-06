7fae381d1696a5edc1bf864edded681fe827c63a
Antoine Ho <34996565+AntoineHo6@users.noreply.github.com>
2021-02-06 14:31:27 -0500

    store page js progress

--- front-end/src/Store.js
+++ front-end/src/Store.js
@@ -1,8 +1,23 @@
+// for (let index = 0; index < 3; index++) {
+//     let clone = document.querySelector('.card').cloneNode( true );
+//     document.querySelector('p').appendChild( clone );
+// }
+
+function allo() {
+    document.querySelector(".cardsContainer").innerHTML += `
+        <div class="card">
+            < img src = "./media/dannyBoy.JPEG" alt = "produit" class="productCardImg" >
+            <div class="cardDescContainer">
+                <h4><b>Sirop d'érable</b></h4>
+            </div>
+        </div > `;
+}
+
 const cards = document.querySelectorAll(".card");
 
 for (let i = 0; i < cards.length; i++) {
     cards[i].addEventListener("click", function () {
-        alert("You clicked this div");
+        allo();
     });
 
 }
