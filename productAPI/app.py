435c1a9b76363b3bd721160201911c6f6ec002fe
LordFarquadTheGreat <moniquetamere1@gmail.com>
2021-02-07 11:26:03 -0500

    Fixes loading order errors

--- productAPI/app.py
+++ productAPI/app.py
@@ -1,8 +1,9 @@
+import time
+time.sleep(5) #TODO find a better solution
+
 import os
 import api
-import time
 from api import flask_app
 
 if __name__ == '__main__':
-    time.sleep(20) #TODO find a better solution
     flask_app.run(host='0.0.0.0', port=8393, debug=True)
