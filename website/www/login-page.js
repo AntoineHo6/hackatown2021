7cdc5cddf968a5efea120b9bcaf07eed389bb19b
Kha Pham <49766423+Mono98ui@users.noreply.github.com>
2021-02-07 08:54:45 -0500

    login page

---
+++ website/www/login-page.js
@@ -0,0 +1,16 @@
+const loginForm = document.getElementById("login-form");
+const loginButton = document.getElementById("login-form-submit");
+const loginMsg = document.getElementById("login-msg");
+
+loginButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    const username = loginForm.username.value;
+    const password = loginForm.password.value;
+
+    if (username === "user" && password === "web_dev") {
+        window.location.replace("./index.html");
+    } else {
+        loginErrorMsg.style.opacity = 1;
+        setTimeout(function(){ loginErrorMsg.style.opacity = 0; }, 3000);
+    }
+})
